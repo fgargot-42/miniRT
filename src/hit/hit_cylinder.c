ad9ddfbc34d6d5c8f6bfca0a17b4a880bb464cba
Florent Gargot <fgargot@3C2.42angouleme.fr>
2026-04-09 16:30:09 +0200

    Fixed cylinder caps render issues

--- src/hit/hit_cylinder.c
+++ src/hit/hit_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 18:57:53 by fgargot           #+#    #+#             */
-/*   Updated: 2026/04/07 23:13:51 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/04/09 16:28:56 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static void	update_hit_record(t_hit_record *rec, t_ray *ray, t_cylinder *cyl,
 
 	oc = vec_apply_rotation_z(vec_sub(ray->origin, cyl->center),
 		cyl->transform_axis);
-	if ((fabs(v_hit.z) - cyl->height / 2.0) <= 1e-6)
+	if ((fabs(v_hit.z) - cyl->height / 2.0) < 1e-6)
 		normal = vec_reverse_rotation(
 				(t_vec3){0, 0, 2 * (v_hit.z > 0) - 1}, cyl->transform_axis);
 	else
@@ -45,7 +45,7 @@ static void	update_hit_record(t_hit_record *rec, t_ray *ray, t_cylinder *cyl,
 				(t_vec3){v_hit.x, v_hit.y, 0}, cyl->transform_axis));
 	rec->t = vec_distance(v_hit, oc);
 	rec->point = ray_at(*ray, rec->t);
-	rec->normal = face_normal(ray, normal);
+	rec->normal = normal;
 	rec->color = cyl->color;
 	rec->object.cylinder = cyl;
 }
@@ -71,7 +71,8 @@ static int	get_intersections(double *roots, t_vec3 *v_hit, t_ray *ray,
 	return (1);
 }
 
-static int	hit_cylinder_cap(t_cylinder *cyl, t_ray *ray, t_vec3 *v_hit, double *roots)
+static int	hit_cylinder_cap(t_cylinder *cyl, t_ray *ray, t_vec3 *v_hit,
+	double *roots)
 {
 	t_vec3	oc;
 	t_vec3	rd;
@@ -87,18 +88,20 @@ static int	hit_cylinder_cap(t_cylinder *cyl, t_ray *ray, t_vec3 *v_hit, double *
 		return (0);
 	if (fabs(oc.z) < cyl->height / 2.0 && fabs(v_hit[1].z) < cyl->height / 2.0)
 		return (1);
-	if (fabs(vec_dot(ray->direction, cyl->axis)) < 1e-6)
-		return (0);
-	cap_z = (2 * (v_hit[0].z > 0) - 1) * cyl->height / 2.0;
-	if (fabs(oc.z) < cyl->height / 2.0)
-		cap_z = (2 * (v_hit[1].z > 0) - 1) * cyl->height / 2.0;
-	z_scale = (cap_z - v_hit[0].z) / (v_hit[1].z - v_hit[0].z);
-	if (fabs(oc.z) < cyl->height / 2.0)
+	if (fabs(oc.z) - cyl->height / 2.0 < 1e-6)
+	{
+		cap_z = (2 * (rd.z > 0) - 1) * cyl->height / 2.0;
 		z_scale = (cap_z - oc.z) / (v_hit[1].z - oc.z);
-	roots[1] = (roots[1] - roots[0]) * z_scale + roots[0];
-	v_hit[1] = vec_add(vec_scale(vec_sub(v_hit[1], v_hit[0]), z_scale), v_hit[0]); 
-	if (fabs(oc.z) < cyl->height / 2.0)
 		v_hit[1] = vec_add(vec_scale(vec_sub(v_hit[1], oc), z_scale), oc); 
+		roots[1] *= z_scale;
+	}
+	else
+	{
+		cap_z = (2 * (v_hit[0].z > 0) - 1) * cyl->height / 2.0;
+		z_scale = (cap_z - v_hit[0].z) / (v_hit[1].z - v_hit[0].z);
+		v_hit[1] = vec_add(vec_scale(vec_sub(v_hit[1], v_hit[0]), z_scale), v_hit[0]);
+		roots[1] = (roots[1] - roots[0]) * z_scale + roots[0];
+	}
 	return (1);
 }
 
