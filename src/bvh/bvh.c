904ba1a8ae17499f3a9a84fc799462621119b005
Florent Gargot <fgargot@3C2.42angouleme.fr>
2026-05-20 18:26:01 +0200

    ADDED: bvh resizing when moving objects (currently may cause other object rendering issues)

--- src/bvh/bvh.c
+++ src/bvh/bvh.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 20:44:12 by fgargot           #+#    #+#             */
-/*   Updated: 2026/05/18 21:55:46 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/05/20 18:15:26 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,4 +47,29 @@ t_bvh	*build_bvh_tree(t_scene *scene)
 	bvh_split(bvh, left_bounds, 0);
 	ft_lstclear(&bvh_objects, NULL);
 	return (bvh);
+}
+
+static void	bvh_destroy_nodes(t_bvh *bvh)
+{
+	if (!bvh)
+		return ;
+	bvh_destroy_nodes(bvh->left);
+	bvh_destroy_nodes(bvh->right);
+	free(bvh);
+}
+
+void	bvh_destroy_tree(t_bvh **bvh)
+{
+	if (!*bvh)
+		return ;
+	free((*bvh)->objects);
+	bvh_destroy_nodes(*bvh);
+	(*bvh)->objects = NULL;
+	*bvh = NULL;
+}
+
+void	rebuild_bvh_tree(t_bvh **bvh, t_scene *scene)
+{
+	bvh_destroy_tree(bvh);
+	*bvh = build_bvh_tree(scene);
 }
---
+++ src/bvh/bvh_size.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bvh_size.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 17:54:19 by fgargot           #+#    #+#             */
+/*   Updated: 2026/05/20 18:09:59 by fgargot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "miniRT.h"
+#include "veclib.h"
+
+void	bvh_grow_to_include(t_bvh *bvh, t_object *object)
+{
+	t_vec3						aabb_min;
+	t_vec3						aabb_max;
+	static const t_obj_aabb_fn	get_aabb[] = {NULL, NULL, NULL, NULL, NULL,
+		get_sphere_aabb, get_cylinder_aabb, get_cone_aabb,
+		get_hyperboloid_aabb, get_paraboloid_aabb, get_triangle_aabb};
+
+	aabb_min = bvh->aabb_min;
+	aabb_max = bvh->aabb_max;
+	if (object->type >= OBJ_SPHERE)
+		get_aabb[object->type](object, &aabb_min, &aabb_max);
+	bvh->aabb_min = vec3_min(bvh->aabb_min, aabb_min);
+	bvh->aabb_max = vec3_max(bvh->aabb_max, aabb_max);
+}
+
+void	bvh_grow_all_to_include(t_bvh *root, t_object *object)
+{
+	int		i;
+	t_bvh	*node;
+
+	i = 0;
+	while (i < root->nb_elements)
+	{
+		if (root->objects[i] == object)
+			break ;
+		i++;
+	}
+	if (i >= root->nb_elements)
+		return ;
+	node = root;
+	while (node)
+	{
+		bvh_grow_to_include(node, object);
+		if (node->right && i >= node->right->first_index)
+			node = node->right;
+		else
+			node = node->left;
+	}
+}
