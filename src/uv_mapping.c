ba1c48a6243b5915c7da5a372156133e968f1a8d
Florent Gargot <fgargot@3C2.42angouleme.fr>
2026-05-23 00:51:31 +0200

    Added: Triangle texture using uv-map data (vt) from obj file

--- src/uv_mapping.c
+++ src/uv_mapping.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 18:21:30 by fgargot           #+#    #+#             */
-/*   Updated: 2026/05/21 23:55:04 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/05/23 00:50:03 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,36 @@ t_vec3 uv_to_color(t_texture *tex, t_vec2 uv, void *mlx)
 	col.y = pixel.g;
 	col.z = pixel.b;
     return (col);
+}
+
+t_vec3	triangle_uv_to_color(t_object *obj, t_vec3 hit, void *mlx)
+{
+	t_vec3		obj_vec[3];
+	double		dot[2][3];
+	double		det;
+	t_vec2		uv;
+	mlx_color	pixel;
+
+	obj_vec[0] = vec3_sub(obj->props.b, obj->props.a);
+	obj_vec[1] = vec3_sub(obj->props.c, obj->props.a);
+	obj_vec[2] = vec3_sub(hit, obj->props.a);
+	dot[0][0] = vec3_dot(obj_vec[0], obj_vec[0]);
+	dot[0][1] = vec3_dot(obj_vec[0], obj_vec[1]);
+	dot[0][2] = vec3_dot(obj_vec[0], obj_vec[2]);
+	dot[1][1] = vec3_dot(obj_vec[1], obj_vec[1]);
+	dot[1][2] = vec3_dot(obj_vec[1], obj_vec[2]);
+	det = dot[0][0] * dot[1][1] - pow(dot[0][1], 2);
+	if (fabs(det) < 1e-10)
+		return (t_vec3){0, 0, 0};
+	uv.x = (dot[1][1] * dot[0][2] - dot[0][1] * dot[1][2]) / det;
+	uv.y = (dot[0][0] * dot[1][2] - dot[0][1] * dot[0][2]) / det;
+	uv = vec2_add(vec2_add(obj->texture.tex_a,
+		vec2_scale(vec2_sub(obj->texture.tex_b, obj->texture.tex_a), uv.x)),
+		vec2_scale(vec2_sub(obj->texture.tex_c, obj->texture.tex_a), uv.y));
+	uv.x = uv.x * obj->sphere_tex.width - 1;
+	uv.y = (1 - uv.y) * obj->sphere_tex.height - 1;
+	pixel = mlx_get_image_pixel(mlx, obj->sphere_tex.data, uv.x, uv.y);
+	return ((t_vec3){pixel.r, pixel.g, pixel.b});
 }
 
 t_texture load_texture(char *path, void *mlx)
