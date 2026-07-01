fc6af940d0f10af101b9e74bd5bee22e8af92720
Florent Gargot <fgargot@student.42angouleme.fr>
2026-07-01 23:39:45 +0200

    fix: camera first rotation issue (pitch/yaw initialization)

--- src/parser/rt_parser/parse_camera.c
+++ src/parser/rt_parser/parse_camera.c
@@ -6,7 +6,7 @@
 /*   By: fgargot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 18:12:11 by fgargot           #+#    #+#             */
-/*   Updated: 2026/05/27 19:03:29 by fgargot          ###   ########.fr       */
+/*   Updated: 2026/07/01 23:37:28 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,8 +45,8 @@ t_object	*parse_camera(char **line_split, t_parser_ctx *ctx, void *mlx)
 		free(obj);
 		return (NULL);
 	}
-	obj->props.pitch = 0.0;
-	obj->props.yaw = 0.0;
+	obj->props.yaw = atan2(obj->direction.z, obj->direction.x) * 180 / M_PI;
+	obj->props.pitch = 90.0 * obj->direction.y;
 	obj->type = OBJ_CAMERA;
 	return (obj);
 }
