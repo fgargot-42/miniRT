f70c7391f575a1c5171b2722f8aca0b98ed6ed36
Florent Gargot <fgargot@student.42angouleme.fr>
2026-07-27 19:46:59 +0200

    fix: font display when editor is open

--- src/ui/ui_light_bonus.c
+++ src/ui/ui_light_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mabarrer <mabarrer@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 19:09:28 by mabarrer          #+#    #+#             */
-/*   Updated: 2026/07/27 19:09:40 by mabarrer         ###   ########.fr       */
+/*   Updated: 2026/07/27 19:46:00 by fgargot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,4 +98,5 @@ void	draw_light_editor(t_data *d)
 	mlx_set_font_scale(d->mlx, "resources/font.ttf", 8.0f);
 	mlx_string_put(d->mlx, d->editor, PANEL_PAD, y + 4,
 		(mlx_color){.rgba = COL_FOOTER}, "fgargot && mabarrer | miniRT");
+	mlx_set_font_scale(d->mlx, "resources/font.ttf", 16.0f);
 }
